6a5c19299f0aff1a3f74e77248e2c082ddcfede3
Yeq6X <orange.m0912@gmail.com>
2024-07-09 06:46:51 +0900

    os.environ('MAX_WORKERS')

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ active_tasks = {}
 task_futures = {}
 
 # ThreadPoolExecutorの作成
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=1) # 8vCPUのインスタンスを使用
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=os.environ.get('MAX_WORKERS', 4))
 
 gpu_lock = threading.Lock()
 
