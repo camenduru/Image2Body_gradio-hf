509663dd4d4a41c48e4a1c6a66dba8652d7eabd0
Yeq6X <orange.m0912@gmail.com>
2024-07-11 15:40:47 +0900

    off lora

--- scripts/process_utils.py
+++ scripts/process_utils.py
@@ -83,10 +83,10 @@ def initialize_sotai_model():
 
     # Create the ControlNet pipeline
     sotai_gen_pipe = StableDiffusionControlNetPipeline(
-        vae=sd_pipe.vae.to(device),
+        vae=sd_pipe.vae,
         text_encoder=sd_pipe.text_encoder,
         tokenizer=sd_pipe.tokenizer,
-        unet=sd_pipe.unet.to(device),
+        unet=sd_pipe.unet,
         scheduler=sd_pipe.scheduler,
         safety_checker=sd_pipe.safety_checker,
         feature_extractor=sd_pipe.feature_extractor,
@@ -99,9 +99,9 @@ def initialize_sotai_model():
         # (os.environ["lora_name2"], 0.3),
     ]
     
-    for lora_name, alpha in lora_names:
-        lora_path = get_file_path(lora_name, subfolder=os.environ["lora_dir"])
-        load_lora(sotai_gen_pipe, lora_path, alpha)
+    # for lora_name, alpha in lora_names:
+    #     lora_path = get_file_path(lora_name, subfolder=os.environ["lora_dir"])
+    #     load_lora(sotai_gen_pipe, lora_path, alpha)
 
     # スケジューラーの設定
     sotai_gen_pipe.scheduler = UniPCMultistepScheduler.from_config(sotai_gen_pipe.scheduler.config)
